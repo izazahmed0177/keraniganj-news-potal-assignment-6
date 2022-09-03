fb08dd3b5211afce92d70651d4c6e8c7376bd1f4
unknown <izazahmed0177@gmail.com>
2022-09-03 12:09:34 +0600

    some responsep

--- js/index.js
+++ js/index.js
@@ -111,31 +111,39 @@ const displayCategoryDetails = async (details) => {
                 <div class="col-md-8">
     
                     <div class="card-body">
-                        <h5 class="card-title">${detail.title}</h5>
-                        <p class="card-text">${detail.details.length > 310 ? detail.details.slice(0,310) + '...' : detail.details}</p>
-                        <div>
-                            <div class="d-flex flex-row mb-3 justify-content-between">
+                      <div>
+                          <h5 class="card-title">${detail.title}</h5>
+                           <p class="card-text">${detail.details.length > 310 ? detail.details.slice(0,310) + '...' : detail.details}</p>
+                       </div>
+                        
+
+                        <div class="" >
+                            <div class="row g-0">
     
                             
-                                <div class="p-2 d-inline ">
-                                    <div class="p-2">
+                             <div class="p-2 row  g-0 col-md-5">
+                                <div class="author">
+                                
+                                  <div class="">
                                         
                                         <img src="${detail.author.img}" class="w-25  rounded-circle " alt="...">
-                                        
-                                        <div class="d-inline">
-                                        <h5>${detail.author.name ? detail.author.name: 'No Author Found'}</h5>
-                                            
-                                            
+                                   </div>
+                                       <div class="">
+                                         <h5>${detail.author.name ? detail.author.name: 'No Author Found'}</h5>
+                                    
+                                          <p>  ${detail.author.published_date ? detail.author.published_date : 'no release Date found'}</p> 
 
-                                            ${detail.author.published_date ? detail.author.published_date : 'no release Date found'}
+                                   
                                         </div>
+                                </div>
+                                   
                                             
                                        
-                                    </div>
-                                </div>
+                             </div>
+                                
     
     
-                                <div class="p-2">
+                                <div class="p-2 col-md-2">
     
                                     <div class="d-flex flex-row">
                                         <i class="bi bi-eye"><svg xmlns="http://www.w3.org/2000/svg"
@@ -145,19 +153,19 @@ const displayCategoryDetails = async (details) => {
                                                     d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                                 <path
                                                     d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                                            </svg></i>
-                                        <p>${detail.total_view} M</p>
+                                            </svg> </i>
+                                        <p> ${detail.total_view} M</p>
                                     </div>
     
                                 </div>
-                                <div class="p-2">
+                                <div class="p-2 col-md-3">
                                     <i class="fa-solid fa-star-half-stroke"></i>
                                     <i class="fa-regular fa-star"></i>
                                     <i class="fa-regular fa-star"></i>
                                     <i class="fa-regular fa-star"></i>
                                     <i class="fa-regular fa-star"></i>
                                 </div>
-                                <div class="p-2">
+                                <div class="p-2 col-md-2">
                                     <button onclick="loadCategoryDetailsModal('${detail._id}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal">Details</button>
                                 </div>
                             </div>
